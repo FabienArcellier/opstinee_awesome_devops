89f093fdc152ec574d45cf00661362a1a70ed10a
Fabien Arcellier <farcellier@octo.com>
2018-08-21 23:21:51 +0200

    add the support of VERSION

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,7 @@
 make html
+git log -n1 --format="$(cat VERSION.in)+git%h" > build/html/VERSION
 cd build/html
 tar -cvf ../../awesome_devops.tar.gz *
 cd ../..
+
 aws s3 cp awesome_devops.tar.gz ${S3_ARTEFACT_REPOSITORY_URL}/awesome_devops.tar.gz
